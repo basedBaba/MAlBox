862d60e6aa0bd6e96532b52e6afab38cb9c487c6
basedBaba <basedBaba@proton.me>
2025-03-01 19:51:35 +0530

    Add capa

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from src.pyghira import decompile_and_objdump
+from src.capa import capa_report
 
 from src.virustotal import virustotal_report
 
@@ -57,6 +58,26 @@ def virustotal():
         return jsonify({"error": f"Processing failed: {str(e)}"}), 500
 
     return jsonify({"analysis_report": analysis_report})
+
+
+@app.route("/capa", methods=["POST"])
+def capa():
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(filepath)
+
+    try:
+        analysis_report = capa_report(filepath)
+    except Exception as e:
+        return jsonify({"error": f"Processing failed: {str(e)}"}), 500
+
+    return jsonify({"capa_report": analysis_report})
 
 
 if __name__ == "__main__":
